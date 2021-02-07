3f8f567e3cd60c5d29c613a5fed6195d5af6ceb0
Pradeep <pt21388@gmail.com>
2021-02-08 01:19:46 +0530

    Update repository with latest framework changes

--- bootstrap/filters.php
+++ bootstrap/filters.php
@@ -8,10 +8,14 @@ $routeFilters = $container->get('router')->filters();
 $filtersConfig = $container->get('config')->filters;
 
 foreach($routeFilters as $filterAlias) {
-    if($filtersConfig[$filterAlias] ?? null) {
-        $filter->register(
-            $container->get('router')->route(), 
-            new $filtersConfig[$filterAlias]
+    if(!isset($filtersConfig[$filterAlias])) {
+        throw new \Lightpack\Exceptions\FilterNotFoundException(
+            "No filter class registered for: {$filterAlias}"
         );
     }
+
+    $filter->register(
+        $container->get('router')->route(), 
+        new $filtersConfig[$filterAlias]
+    );
 }
